a90c0c3d21675fc400cf9f73b0524f0c839d5399
alonglo361 <alonglo361@nemi.tskoli.is>
2018-11-08 04:31:20 +0000

    Update Verkefni4a.cpp

--- Verkefni4a.cpp
+++ Verkefni4a.cpp
@@ -1,45 +1,52 @@
 #include <iostream>
 #include <sstream>
-#include <iomanip> //Nota iomanip til þess að fá setprecision.
+#include <iomanip> //Nota iomanip til Ã¾ess aÃ° fÃ¡ setprecision.
 #include <math.h> //Nota math.h fyrir ceil.
 using namespace std;
 
-class FlightBooking {
-public:
-	FlightBooking(int id, int capacity, int reserved);
-	void printStatus();
-	bool reserveSeats(int flightID, int number_of_seats);
-	bool cancelReservations(int flightID, int number_of_seats);
+class FlightBooking { // Allar breytur
 private:
 	int id;
 	int capacity;
 	int reserved;
+public:
+	FlightBooking(int id = 0, int capacity = 0, int reserved = 0) // Constructor
+  {
+    this->id = id;
+    this->capacity = capacity;
+    this->reserved = reserved;
+  }
+
+	void printStatus(); // Ã–ll fÃ¶ll sem Ã©g nota
+  int returnID();
+	bool reserveSeats(int flightID, int number_of_seats);
+	bool cancelReservations(int flightID, int number_of_seats);
 };
+
+int FlightBooking::returnID()
+{
+  return FlightBooking::id; //Ãžetta er notaÃ° til aÃ° bera saman ID.
+}
 
 void FlightBooking::printStatus()
 {
 
-	float procentage = (float(reserved) / float(capacity)) * 100; //Breyti bæði breytunum í float svo að ég get reiknað prósentuna.
+	float procentage = (float(reserved) / float(capacity)) * 100; //Breyti bÃ¦Ã°i breytunum Ã­ float svo aÃ° Ã©g get reiknaÃ° prÃ³sentuna.
 	cout << "\n" << "Flight " << id << " : " << reserved << "/" << capacity << " (" << fixed << setprecision(0) << procentage << "%)  seats reserved" << "\n\n";
-	//Kalla þessa runu til þess að prenta út allt sem ég þarf + líka prósentutöluna með því að nota "setprecision(0)" til að round-a töluna
-	//svo að prósentan verður ekki með neina aukastafi, og "fixed" til þess að fá 'fixed floating-point notation'.
+	//Kalla Ã¾essa runu til Ã¾ess aÃ° prenta Ãºt allt sem Ã©g Ã¾arf + lÃ­ka prÃ³sentutÃ¶luna meÃ° Ã¾vÃ­ aÃ° nota "setprecision(0)" til aÃ° round-a tÃ¶luna
+	//svo aÃ° prÃ³sentan verÃ°ur ekki meÃ° neina aukastafi, og "fixed" til Ã¾ess aÃ° fÃ¡ 'fixed floating-point notation'.
 }
-FlightBooking::FlightBooking(int id, int capacity, int reserved)
-{
-	FlightBooking::id = id;
-	FlightBooking::capacity = capacity;
-	FlightBooking::reserved = reserved;
-}
+
 
 bool FlightBooking::reserveSeats(int flightID, int number_of_seats)
 {
-	float newReserved = float(reserved) + float(number_of_seats); //Geri nýja breytu með newReserved til þess að sleppa því að skoða tvisvar hvort það sé pláss fyrir fleiri reservations.
+	float newReserved = float(reserved) + float(number_of_seats); //Geri nÃ½ja breytu meÃ° newReserved til Ã¾ess aÃ° sleppa Ã¾vÃ­ aÃ° skoÃ°a tvisvar hvort Ã¾aÃ° sÃ© plÃ¡ss fyrir fleiri reservations.
 	float procentage = ceil((float(newReserved) / float(capacity)) * 100);
-	//Þessi breyta er notuð fyrir neðan til að skoða hvort það sé hægt að bæta við auka reservations.
-	//Ég nota líka "ceil" til að round-a niður svo að það verður ekki 15.x prósent. (Ég mátti ráða að nota floor eða ceiling.)
+	//Ãžessi breyta er notuÃ° fyrir neÃ°an til aÃ° skoÃ°a hvort Ã¾aÃ° sÃ© hÃ¦gt aÃ° bÃ¦ta viÃ° auka reservations.
+	//Ã‰g nota lÃ­ka "ceil" til aÃ° round-a niÃ°ur svo aÃ° Ã¾aÃ° verÃ°ur ekki 15.x prÃ³sent. (Ã‰g mÃ¡tti rÃ¡Ã°a aÃ° nota floor eÃ°a ceiling.)
 	if (procentage <= 105)
 	{
-		FlightBooking::reserved += number_of_seats; //Bæti við reservations.
+		FlightBooking::reserved += number_of_seats; //BÃ¦ti viÃ° reservations.
 		return true;
 	}
 	else
@@ -48,44 +55,40 @@ bool FlightBooking::reserveSeats(int flightID, int number_of_seats)
 
 bool FlightBooking::cancelReservations(int flightID, int number_of_seats)
 {
-	int checkReserved = (reserved - number_of_seats); //Nota þessa breytu fyrir neðan til þess að skoða hvort ég er með nóg af resrvations til þess að fjarlæga í burtu.
+	int checkReserved = (reserved - number_of_seats); //Nota Ã¾essa breytu fyrir neÃ°an til Ã¾ess aÃ° skoÃ°a hvort Ã©g er meÃ° nÃ³g af resrvations til Ã¾ess aÃ° fjarlÃ¦ga Ã­ burtu.
 	if (checkReserved >= 0)
 	{
-		FlightBooking::reserved -= number_of_seats; //Fjarlægi reservations ef það eru ekki undir 0.
+		FlightBooking::reserved -= number_of_seats; //FjarlÃ¦gi reservations ef Ã¾aÃ° eru ekki undir 0.
 		return true;
 	}
-	else if (checkReserved < 0) //Ef sætin eru negatíf eftir það er búið að fjarlæga frá, vistast sætin sem 0.
+	else if (checkReserved < 0) //Ef sÃ¦tin eru negatÃ­f eftir Ã¾aÃ° er bÃºiÃ° aÃ° fjarlÃ¦ga frÃ¡, vistast sÃ¦tin sem 0.
 	{
 		FlightBooking::reserved = 0;
 		return true;
 	}
-	else //Ef eitthvað fer úrskeiðis.
+	else //Ef eitthvaÃ° fer ÃºrskeiÃ°is.
 		return false;
 }
 
 
 int main() {
+
 	string failedString = "\n Cannot perform this operation \n\n";
-	string fEdit; // Strengur sem heldur á user-input. fEdit stendur fyrir "Flight Edit"
-	bool loop = true;
-	int reserved = 0,
-		capacity = 0;
-	cout << "Provide flight capacity: ";
-	cin >> capacity;
-	cout << "Provide number of reserved seats: ";
-	cin >> reserved;
-
-	FlightBooking booking(1, capacity, reserved);
-
-	booking.printStatus();
+	string fEdit; // Strengur sem heldur Ã¡ user-input. fEdit stendur fyrir "Flight Edit"
+	bool loop = true; // Keyrir aÃ°al-forritiÃ°.
+  int arrsize = 2, counter = 0; // BÃ½ til teljaran og array-size.
+	FlightBooking* booking = new FlightBooking[arrsize]; // BÃ½ til array-iÃ° meÃ° tvÃ¶ stÃ¶k.
 
 	while (loop == true)
 	{
 		cout << "----------------------------------- \n";
 		cout << "Welcome User. \n";
-		cout << "You have 3 procedures available: \n";
+		cout << "You have 6 procedures available: \n";
+    cout << "create [ID] [integer] \n";
+    cout << "delete [ID] \n";
 		cout << "add [ID] [integer] \n";
 		cout << "cancel [ID] [integer] \n";
+    cout << "show \n";
 		cout << "quit" << "\n \n";
 		cout << "An example procedure would be: \n";
 		cout << "add 1 4 \n";
@@ -93,218 +96,95 @@ int main() {
 		cout << "cancel 3 20 \n";
 		cout << "----------------------------------- \n";
 
-		cout << "Choose Procedure: ";
+		cout << "Choose Procedure:";
 		getline(cin, fEdit);
 
 		int id; // Velja flight number ID
-		int cap; // Hversu marga á að taka eða setja inn.
-		string procedure; // Taka fram hvað procedure ég nota.
-		stringstream procedureString; // Setja svo í string stream.
+		int cap; // Hversu marga Ã¡ aÃ° taka eÃ°a setja inn.
+    int whichID; // Segir hvaÃ°a ID viÃ° erum aÃ° vinna meÃ°
+    bool exists = false; // SkoÃ°ar hvort Ã¾etta ID er til.
+		string procedure; // Taka fram hvaÃ° procedure Ã©g nota.
+		stringstream procedureString; // Setja svo Ã­ string stream.
 		procedureString << fEdit;
 		procedureString >> procedure >> id >> cap;
-		if (procedure == "add") // Fyrir að bæta við
+
+    for (int i = 0; i < arrsize; i++) //SkoÃ°ar Ã­ hvert sinn hvort ID-iÃ° er til Ã­ array.
+    {
+      if (booking[i].returnID() == id)
+      {
+        whichID = i; // Segir hvaÃ°a stak Ã¡ aÃ° taka til aÃ° fÃ¡ Ã¾etta ID ef Ã¾aÃ° er til.
+        exists = true;
+      }
+    }
+		if (procedure == "add" && exists == true) // Fyrir aÃ° bÃ¦ta viÃ°
 		{
-			if (booking.reserveSeats(id, cap) == true) // Þessi if-setning keyrir fallið í rauninni, þannig að ég þarf ekki að kalla það tvisvar, og þá fæ ég líka að sjá hvort það er true eða false.
-				booking.printStatus();
+			if (booking[whichID].reserveSeats(id, cap) == true) // Ãžessi if-setning keyrir falliÃ° Ã­ rauninni, Ã¾annig aÃ° Ã©g Ã¾arf ekki aÃ° kalla Ã¾aÃ° tvisvar, og Ã¾Ã¡ fÃ¦ Ã©g lÃ­ka aÃ° sjÃ¡ hvort Ã¾aÃ° er true eÃ°a false.
+				booking[whichID].printStatus();
 
 			else
-				cout << failedString; //Prentar út fail string þegar það virkar ekki.
+				cout << failedString; //Prentar Ãºt fail string Ã¾egar Ã¾aÃ° virkar ekki.
 		}
 
-		else if (procedure == "cancel") // Fyrir að taka frá.
+		else if (procedure == "cancel" && exists == true) // Fyrir aÃ° taka frÃ¡.
 		{
-			if (booking.cancelReservations(id, cap) == true)
-				booking.printStatus();
+			if (booking[whichID].cancelReservations(id, cap) == true)
+				booking[whichID].printStatus();
 
 			else
 				cout << failedString;
 		}
 
-		else if (procedure == "quit") // Fer úr loop, og hættir að keyra forrit.
+    else if (procedure == "create" && exists == false) //BÃ½r til nÃ½tt object, skoÃ°ar fyrst ef Ã¾aÃ° sÃ© til eÃ°a ekki.
+    {
+      if (arrsize <= counter) //SkoÃ°ar hvort array-iÃ° er eins stÃ³rt eÃ°a minna, og ef Ã¾aÃ° er Ã¾annig, Ã¾Ã¡ er tvÃ¶faldaÃ° stÃ¦rÃ°in Ã¡ array meÃ° Ã¾vÃ­ aÃ° nota temp_array.
+      {
+        FlightBooking* tmp_array = new FlightBooking[arrsize];
+
+        for (int k = 0; k < counter; k++)
+          tmp_array[k] = booking[k];
+      
+        delete[]booking;
+
+        arrsize *= 2;
+        booking = new FlightBooking[arrsize];
+        for (int k = 0; k < counter; k++)
+          booking[k] = tmp_array[k];
+        
+        delete[]tmp_array;
+      }
+
+      booking[counter] = FlightBooking(id, cap, 0);
+      booking[counter].printStatus();
+      counter += 1;
+
+    }
+
+    else if (procedure == "delete" && exists == true) //Hendir Ãºt stakinu meÃ° Ã¾vÃ­ aÃ° fÃ¦ra allt til vinstri og svo henda counter-inum Ãºt Ã¾annig aÃ° hann mun aldrei teljast meÃ°. Ef Ã¾aÃ° kemur nÃ½tt stak, Ã¾Ã¡ mun Ã¾aÃ° skrifa yfir gamla.
+    {
+      for (int i = whichID; i < counter; i++)
+        booking[i] = booking[i+1];
+      counter -= 1;
+      cout << "Successfully deleted. \n";
+      
+    }
+
+		else if (procedure == "quit") // Fer Ãºr loop, og hÃ¦ttir aÃ° keyra forrit.
 		{
 			cout << "Logging out of service.";
 			loop = false;
 
 		}
-		else
+
+    else if (procedure == "show") // SÃ½nir Ã¶ll flug.
+    {
+      for (int i = 0; i < counter; i++)
+        booking[i].printStatus();
+    }
+
+		else // Ef eitthvaÃ° fer ÃºrskeiÃ°is Ã¾Ã¡ prentar Ã¾aÃ° Ãºt error.
 			cout << failedString;
-	}
-
-	return 0;
-}
-
-
-
-// NEW CODE HERE
-// NEW
-// NEW
-// NEW 
-// NEW
-// NEW
-// NEW 
-// NEW
-
-
-#include <iostream>
-#include <sstream>
-#include <iomanip> //Nota iomanip til þess að fá setprecision.
-#include <math.h> //Nota math.h fyrir ceil.
-using namespace std;
-
-class FlightBooking {
-public:
-	FlightBooking(int id = 0, int capacity = 0, int reserved = 0);
-	FlightBooking();
-	void createFlight();
-	void printStatus();
-	bool reserveSeats(int flightID, int number_of_seats);
-	bool cancelReservations(int flightID, int number_of_seats);
-	int getID(int flightID);
-
-private:
-	int id;
-	int capacity;
-	int reserved;
-};
-
-
-FlightBooking::FlightBooking(int id, int capacity, int reserved)
-{
-	FlightBooking::id = id;
-	FlightBooking::capacity = capacity;
-	FlightBooking::reserved = reserved;
-}
-
-void FlightBooking::printStatus()
-{
-
-	float procentage = (float(reserved) / float(capacity)) * 100; //Breyti bæði breytunum í float svo að ég get reiknað prósentuna.
-	cout << "\n" << "Flight " << id << " : " << reserved << "/" << capacity << " (" << fixed << setprecision(0) << procentage << "%)  seats reserved" << "\n\n";
-	//Kalla þessa runu til þess að prenta út allt sem ég þarf + líka prósentutöluna með því að nota "setprecision(0)" til að round-a töluna
-	//svo að prósentan verður ekki með neina aukastafi, og "fixed" til þess að fá 'fixed floating-point notation'.
-}
-
-bool FlightBooking::reserveSeats(int flightID, int number_of_seats)
-{
-	float newReserved = float(reserved) + float(number_of_seats); //Geri nýja breytu með newReserved til þess að sleppa því að skoða tvisvar hvort það sé pláss fyrir fleiri reservations.
-	float procentage = ceil((float(newReserved) / float(capacity)) * 100);
-	//Þessi breyta er notuð fyrir neðan til að skoða hvort það sé hægt að bæta við auka reservations.
-	//Ég nota líka "ceil" til að round-a niður svo að það verður ekki 15.x prósent. (Ég mátti ráða að nota floor eða ceiling.)
-	if (procentage <= 105)
-	{
-		FlightBooking::reserved += number_of_seats; //Bæti við reservations.
-		return true;
-	}
-	else
-		return false;
-}
-
-bool FlightBooking::cancelReservations(int flightID, int number_of_seats)
-{
-	int checkReserved = (reserved - number_of_seats); //Nota þessa breytu fyrir neðan til þess að skoða hvort ég er með nóg af resrvations til þess að fjarlæga í burtu.
-	if (checkReserved >= 0)
-	{
-		FlightBooking::reserved -= number_of_seats; //Fjarlægi reservations ef það eru ekki undir 0.
-		return true;
-	}
-	else if (checkReserved < 0) //Ef sætin eru negatíf eftir það er búið að fjarlæga frá, vistast sætin sem 0.
-	{
-		FlightBooking::reserved = 0;
-		return true;
-	}
-	else //Ef eitthvað fer úrskeiðis.
-		return false;
-}
-
-int FlightBooking::getID(int flightID)
-{
-	return this->id;
-}
-
-
-int main() {
-	int arrsize = 1; // Heldur á array-size.
-	int counter = 0; // Heldur á value í array.
-	string failedString = "\n Cannot perform this operation \n\n";
-	string fEdit; // Strengur sem heldur á user-input. fEdit stendur fyrir "Flight Edit"
-	bool loop = true;
-	int reserved = 0,
-		capacity = 0;
-	cout << "Provide flight capacity: ";
-	cin >> capacity;
-	cout << "Provide number of reserved seats: ";
-	cin >> reserved;
-
-	FlightBooking id1(1, capacity, reserved);
-	FlightBooking *flightIDs[1]; // Bý til nýtt array.
-	flightIDs[0] = new FlightBooking(1, capacity, reserved);
-	counter += 1;
-	id1.printStatus();
-	flightIDs[0]->printStatus();
-
-	while (loop == true)
-	{
-		cout << "----------------------------------- \n";
-		cout << "Welcome User. \n";
-		cout << "You have 3 procedures available: \n";
-		cout << "add [ID] [integer] \n";
-		cout << "cancel [ID] [integer] \n";
-		cout << "quit" << "\n \n";
-		cout << "An example procedure would be: \n";
-		cout << "add 1 4 \n";
-		cout << "or \n";
-		cout << "cancel 3 20 \n";
-		cout << "----------------------------------- \n";
-
-		cout << "Choose Procedure: ";
-		getline(cin, fEdit);
-
-		int id; // Velja flight number ID
-		int cap; // Hversu marga á að taka eða setja inn.
-		string procedure; // Taka fram hvað procedure ég nota.
-		stringstream procedureString; // Setja svo í string stream.
-		procedureString << fEdit;
-		procedureString >> procedure >> id >> cap;
-
-		if (procedure == "create")
-		{
-			bool idExist = false; // Boolean sem segir hvort id er til.
-			for (unsigned int i = 0; i < sizeof(flightIDs) / sizeof(flightIDs[0]); i++)
-			{
-				if ((flightIDs[i]->getID(id)) == id)
-				{
-					idExist = true;
-				}
-			}
-
-		}
-
-		else if (procedure == "add") // Fyrir að bæta við
-		{
-			if (id1.reserveSeats(id, cap) == true) // Þessi if-setning keyrir fallið í rauninni, þannig að ég þarf ekki að kalla það tvisvar, og þá fæ ég líka að sjá hvort það er true eða false.
-				id1.printStatus();
-
-			else
-				cout << failedString; //Prentar út fail string þegar það virkar ekki.
-		}
-
-		else if (procedure == "cancel") // Fyrir að taka frá.
-		{
-			if (id1.cancelReservations(id, cap) == true)
-				id1.printStatus();
-
-			else
-				cout << failedString;
-		}
-
-		else if (procedure == "quit") // Fer úr loop, og hættir að keyra forrit.
-		{
-			cout << "Logging out of service.";
-			loop = false;
-
-		}
-		else
-			cout << failedString;
+    
+    exists = false; //Endurstilla "exists" breytuna.
 	}
 
 	return 0;
